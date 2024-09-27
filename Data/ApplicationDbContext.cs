4c041567e5b13fe87965c501e2bcdbd33a9fce5f
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-27 12:28:14 +0530

    initial setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAD_Backend_Application__.NET.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
